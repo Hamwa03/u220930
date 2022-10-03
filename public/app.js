11a32b9a1e572c61c4758dc1fb9db4232c7b70ad
hamwa03 <hampus.hjw.wallin@gmail.com>
2022-10-03 10:44:56 +0200

    created the structute in js with structure.onload and a for loop

--- public/app.js
+++ public/app.js
@@ -1,9 +1,31 @@
+console.log('stable code');
+
+
 const structure = new XMLHttpRequest();
 
 structure.open('GET', 'structure.json');
 structure.send();
 
+
 const cars = new XMLHttpRequest();
 
 cars.open('GET', 'cars.json');
 cars.send();
+
+
+structure.onload = () => {
+
+    var divTitle = document.getElementById('title');
+
+    var headerInfo = JSON.parse(structure.responseText);
+
+    for (var key in headerInfo) {
+
+        var header = document.createElement(key);
+
+        header.innerHTML = headerInfo[key];
+
+        divTitle.appendChild(header);
+    }
+
+}
